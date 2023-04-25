87c5093f9ccbb8eb2918e5f0ca75a48d4c60d04a
Tanushree Banerjee <tb21@cs.princeton.edu>
2023-04-24 21:26:45 -0400

    added configs

---
+++ utils/configs.py
@@ -0,0 +1,6 @@
+ROOT = ".."
+
+DEVICE = "cpu"
+
+KINETICS_400_DATASET_URL = "https://dl.fbaipublicfiles.com/pyslowfast/dataset/class_names/kinetics_classnames.json"
+KINETICS_400_DATASET_FILENAME = f"{ROOT}/data/kinetics_classnames.json"
