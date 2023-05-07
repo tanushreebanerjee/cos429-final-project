0a8b9917577e70a17878df66929cfc15605b31be
Brian Lou <blou@princeton.edu>
2023-05-07 14:32:57 -0400

    Update some code

--- clip_sampler.py
+++ clip_sampler.py
@@ -1,6 +1,7 @@
 import numpy as np
 import av
 from utils import get_frames_from_container, get_all_frames_from_container
+from utils import save_pickle_file, get_flow_from_frames
 import random
 # random.seed(0)
 # np.random.seed(0)
@@ -29,7 +30,7 @@ def sample_obj_detection_nonseq(images, num_frams):
         num_objects_list[i] = num_objects
     return images[np.sort(np.argpartition(num_objects_list, num_frams)[:num_frams])]
   
-        
+  
 def sample_random_indices(clip_len, frame_sample_rate, seg_len):
     converted_len = int(clip_len * frame_sample_rate)
     end_idx = np.random.randint(converted_len, seg_len)
@@ -40,8 +41,11 @@ def sample_random_indices(clip_len, frame_sample_rate, seg_len):
 
 def truly_random_indices(num_frames, frame_sample_rate, seg_len):
     random_list = random.sample(range(0, seg_len), num_frames)
-    random_list.sort()
-    return random_list
+    indices = np.repeat(random_list, 16/num_frames)
+    indices.sort()
+    return indices
+
+
 
 def sample_fourths(num_frames, frame_sample_rate, seg_len):
     one = random.randint(0, int(0.25 * seg_len))
@@ -52,8 +56,55 @@ def sample_fourths(num_frames, frame_sample_rate, seg_len):
     final = ([one] * 4) + ([two] * 4) + ([three] * 4) + ([four] * 4)
     return final
 
+def uniform_sample_indices(seg_len, percent_of_frames):
+  num_examples = percent_of_frames * seg_len
+  indices = np.arange(start = 0, stop = seg_len, step = int(seg_len/num_examples))
+  return indices
 
-def get_frames_from_video_path(frame_type, video_path, num_frames, frame_sample_rate, obj_detection_model = None):
+def sample_optical_flow_indices(frame_type, video_path, seg_len, container, flow_dict, flow_id_dict, force_reload=False):
+  #0. Check if frames are cached 
+  flow_values = []
+  #These are the indices of the frames from which the flow was calculated
+  flow_values_frame_ids = []
+  video_path = str(video_path)
+
+  if video_path in flow_dict and not force_reload:
+    #flow values
+    flow_values = flow_dict[video_path]
+    flow_values_frame_ids = flow_id_dict[video_path]
+  else:
+    #1. Uniformly Sample 50% of the Frames
+    flow_values_frame_ids = uniform_sample_indices(seg_len, 0.5)
+    frames = get_frames_from_container(container, flow_values_frame_ids, format_type = "gray")
+    #2. Calculate Flow on Those Frames
+    flow_values = get_flow_from_frames(frames)
+    flow_values_frame_ids = flow_values_frame_ids[1:]
+    flow_dict[str(video_path)] = flow_values
+    flow_id_dict[str(video_path)] = flow_values_frame_ids
+    save_pickle_file(flow_dict, "flow_dict.pickle")
+    save_pickle_file(flow_id_dict, "flow_id_dict.pickle")
+
+  # #3. Sample Frames Based on Flow
+  if frame_type == "optical-flow-top16":
+    flow_indices = np.argpartition(flow_values,-16)[-16:]
+    flow_indices.sort()
+  elif frame_type == "optical-flow-low16":
+    flow_indices = np.argpartition(flow_values, 16)[:16]
+    flow_indices.sort()
+  elif frame_type == "optical-flow-mixed":
+    highest_flow_indices = np.argpartition(flow_values,-8)[-8:]
+    lowest_flow_indices = np.argpartition(flow_values, 8)[:8]
+    flow_indices = np.concatenate((lowest_flow_indices,highest_flow_indices),axis=0)
+    flow_indices.sort()
+  elif frame_type == "optical-flow-one":
+    print("Eight")
+    highest_flow_indices = np.argpartition(flow_values,-8)[-8:]
+    flow_indices = np.repeat(highest_flow_indices, 2)
+    flow_indices.sort()
+
+  return [flow_values_frame_ids[x] for x in flow_indices]
+
+def get_frames_from_video_path(frame_type, video_path, num_frames, frame_sample_rate, flow_dict, flow_id_dict):
     container = av.open(str(video_path.resolve()))
     seg_len = container.streams.video[0].frames
 
@@ -70,6 +121,8 @@ def get_frames_from_video_path(frame_type, video_path, num_frames, frame_sample_
       return get_all_frames_from_container(container)
     elif frame_type == "obj_detction_nonseq":
       return sample_obj_detection_nonseq(get_all_frames_from_container(container), num_frames)
+    elif "optical-flow" in frame_type:
+      indices = sample_optical_flow_indices(frame_type, video_path, seg_len, container, flow_dict, flow_id_dict)
     
     frames = get_frames_from_container(container, indices)
     return frames
--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import av
 import numpy as np
+import cv2 as cv
+import time 
+import pickle
 
 def get_frames_from_container(container, indices, format_type="rgb24"):
   '''
@@ -28,7 +31,6 @@ def get_frames_from_container(container, indices, format_type="rgb24"):
   return np.stack([x.to_ndarray(format=format_type, height = 360, width=480) for x in frames])
 
 
- 
 def get_all_frames_from_container(container):
   frames = []
   container.seek(0)
@@ -36,4 +38,22 @@ def get_all_frames_from_container(container):
     frames.append(frame)
   return np.stack([x.to_ndarray(format="rgb24") for x in frames])
 
+def save_pickle_file(obj, file_name):
+  with open(file_name, 'wb') as handle:
+    pickle.dump(obj, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
+def load_pickle_file(file_name):
+  with open(file_name, 'rb') as handle:
+      return pickle.load(handle)
+
+def uniform_sample_indices(seg_len, percent_of_frames):
+  num_examples = percent_of_frames * seg_len
+  indices = np.arange(start = 0, stop = seg_len, step = int(seg_len/num_examples))
+  return indices
+
+def get_flow_from_frames(frames):
+  flow_values = []
+  for i in range(0, len(frames) - 1):
+    flow = np.linalg.norm(cv.calcOpticalFlowFarneback(frames[i], frames[i + 1], None, 0.5, 3, 15, 3, 5, 1.2, 0))
+    flow_values.append(flow)
+  return flow_values
